390652560d42a8fe7528926b67aa644f1e4ee154
Seik Oh <50728605+glitterer@users.noreply.github.com>
2022-07-11 15:03:45 +0900

    Create 1544. Make The String Great.py

---
+++ Stack/1544. Make The String Great.py
@@ -0,0 +1,10 @@
+class Solution:
+    def makeGood(self, s: str) -> str:
+        stack = []
+        for i in s:
+            stack.append(i)
+            if len(stack)>1:
+                if stack[-1] != stack[-2] and stack[-1].lower() == stack[-2].lower():
+                    stack.pop()
+                    stack.pop()
+        return (''.join(stack))
