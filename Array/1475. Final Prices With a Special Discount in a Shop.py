fd02cad2c70614ee26e46d9cb87ac5fae498de87
Seik Oh <50728605+glitterer@users.noreply.github.com>
2022-07-16 22:40:17 +0900

    Update 1475. Final Prices With a Special Discount in a Shop.py

--- Array/1475. Final Prices With a Special Discount in a Shop.py
+++ Array/1475. Final Prices With a Special Discount in a Shop.py
@@ -13,3 +13,35 @@ class Solution:
                 ans.append(prices[i])
         ans.append(prices[prices_len-1])
         return ans
+        
+        '''
+        # answer from Discussion
+        class Solution:
+            def finalPrices(self, prices: List[int]) -> List[int]:
+                mono = []
+
+                for i, p in enumerate(prices):
+                    while mono and prices[mono[-1]] >= p:
+                        prices[mono.pop()] -= p
+                    mono.append(i)
+                return prices
+        '''
+                
+        '''
+        # Stack
+        ans = []
+        stack = []
+        for i in range(len(prices)):
+            cnt = 1
+            while cnt != len(prices)-i:
+                stack.append(prices[i+cnt])
+                if stack[-1] <= prices[i]:
+                    ans.append(abs(prices[i]-stack[-1]))
+                    stack.pop()
+                    break
+                stack.pop()
+                cnt += 1
+            if len(ans) != i+1:
+                ans.append(prices[i])
+        return ans   
+        '''
