6bbd95ec6b4e398e853bb17a5d8f42569685af52
Seik Oh <50728605+glitterer@users.noreply.github.com>
2022-07-30 17:15:50 +0900

    1480. Running Sum of 1d Array에 재귀로 푸는 방법 추가

--- Array/1480. Running Sum of 1d Array.py
+++ Array/1480. Running Sum of 1d Array.py
@@ -1,9 +1,24 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        final_ans = []
+        '''
+        def runningSum(self,nums,i=1):
+            if(i==len(nums)):
+                return nums[:]
+            nums[i]+=nums[i-1]
+            return self.runningSum(nums,i+1)
+        '''
+        
+        # final_ans = []
+        # for i in range(len(nums)):
+        #     ans_1 = []
+        #     for j in range(i+1):
+        #         ans_1.append(nums[j])
+        #     final_ans.append(sum(ans_1))
+        # return final_ans
+    
         for i in range(len(nums)):
-            ans_1 = []
-            for j in range(i+1):
-                ans_1.append(nums[j])
-            final_ans.append(sum(ans_1))
-        return final_ans
+            if i == 0:
+                nums[i] = nums[0]
+            else:
+                nums[i] = nums[i-1] + nums[i]
+        return nums
