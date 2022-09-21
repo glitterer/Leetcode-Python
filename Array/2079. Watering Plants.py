745c4258865676745a4d794e1b8ce4261963e414
Seik Oh <50728605+glitterer@users.noreply.github.com>
2022-09-21 23:58:55 +0900

    Update 2079. Watering Plants.py

--- Array/2079. Watering Plants.py
+++ Array/2079. Watering Plants.py
@@ -10,3 +10,20 @@ class Solution:
                 remain = capacity # refill
             remain -= plants[i]
         return steps
+
+'''
+class Solution:
+    def wateringPlants(self, plants: List[int], capacity: int) -> int:
+        remain = capacity
+        steps = 0
+        for i in range(len(plants)):
+            if remain >= plants[i]:
+                remain = remain - plants[i]
+                steps += 1 # 다음 plant
+            else:
+                steps += i # river로 돌아가기
+                steps += i+1 # plant로 돌아감
+                remain = capacity # refill
+                remain = remain - plants[i]
+        return steps
+'''
